c52f49566463a75b13ebff4533763484d73c261a
abhinavdas1 <iabhinavdas@gmail.com>
2016-11-22 03:44:52 -0500

    initial changes

--- src/contigiousMinimumUnsorted.java
+++ src/contigiousMinimumUnsorted.java
@@ -6,7 +6,7 @@ public class contigiousMinimumUnsorted {
         int startIndex = 0;
         int endIndex = A.length-1;
 
-        for(int i = 1; i < A.length;i++){
+        for(int i = 1; i < A.length; i++){
             if(A[i] < A[i-1]){
                 startIndex = i-1;
                 break;
